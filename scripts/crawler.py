0b7b903112e8dd992ac7d07fc07a71beaac0bd63
ksj1368 <ksj006151@gmail.com>
2025-04-26 01:37:32 +0900

    add: 장비, 특성, 캐릭터 정보 크롤러 추가

--- scripts/crawler.py
+++ scripts/crawler.py
@@ -13,8 +13,57 @@ HEADERS_WITH_KEY = {
     "accept": "application/json",
     "x-api-key": API_KEY
 }
+def get_character():
 
+    character_url = f'https://open-api.bser.io/v2/data/Character'
+    character_levelup_url = f'https://open-api.bser.io/v2/data/CharacterLevelUpStat'
+    response_c = requests.get(character_url, headers=HEADERS_WITH_KEY)
+    response_cl = requests.get(character_levelup_url, headers=HEADERS_WITH_KEY)
+    if response_c.status_code == 200 and response_cl.status_code == 200:
+        return response_c.json(), response_cl.json()
+    else:
+        print(f"[Error] get_character - status_code: {response_c.status_code}")
+        print(f"[Error] get_character - status_code: {response_cl.status_code}")
+        return None
 
+def get_equipment():
+
+    url_armor = f'https://open-api.bser.io/v2/data/ItemArmor'
+    url_weapon = f'https://open-api.bser.io/v2/data/ItemWeapon'
+    
+    response_armor = requests.get(url_armor, headers=HEADERS_WITH_KEY)
+    response_weapon = requests.get(url_weapon, headers=HEADERS_WITH_KEY)
+    if response_armor.status_code == 200 and response_weapon.status_code == 200:
+        return response_armor.json(), response_weapon.json()
+    else:
+        print(f"[Error] get_equipment armor - status_code: {response_armor.status_code}")
+        print(f"[Error] get_equipment weapon - status_code: {response_weapon.status_code}")
+        return None
+    
+def get_trait():
+
+    url = f'https://open-api.bser.io/v2/data/Trait'
+    
+    response = requests.get(url, headers=HEADERS_WITH_KEY)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"[Error] get_trait - status_code: {response.status_code}")
+        return None
+def get_l10n():
+    url = 'https://d1wkxvul68bth9.cloudfront.net/l10n/l10n-Korean-20250417055750.txt'
+    response = requests.get(url, headers=HEADERS_WITH_KEY)
+        
+    if response.status_code == 200:
+        response.encoding = response.apparent_encoding
+        l10n = response.text
+        return l10n
+    else:
+        print(f"[Error] get_trait - status_code: {response.status_code}")
+        return None
+
+    
 def get_top_ranker(season: int, matching_mode: int) -> dict | None:
     """
     특정 시즌, 지역, 매칭 모드에서의 상위 랭커 정보를 반환합니다.
